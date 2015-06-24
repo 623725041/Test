ac66ad39cd85d2d0822bf45efca8e777f3cf8af0
Shuai <623725041@qq.com>
2015-06-24 09:56:44 +0800

    新增的文件

---
+++ Git练习/MyView.m
@@ -0,0 +1,21 @@
+//
+//  MyView.m
+//  Git练习
+//
+//  Created by lanouhn on 15/6/24.
+//  Copyright (c) 2015年 Shuai168. All rights reserved.
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
